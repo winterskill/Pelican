710c97b51f049bd92c2e219517b6d5eaabf0482f
Winterskill <kowalski.hanguscorporation@gmail.com>
2018-02-20 17:49:14 +0100

    Création des bases
    Rédaction des bases du bot.

---
+++ inviteBot.cmd
@@ -0,0 +1,2 @@
+start https://discordapp.com/oauth2/authorize?client_id=413019476112703498"&scope=bot&permissions=8"
+exit
--- src/winterskill/discord/pelican/Main.java
+++ src/winterskill/discord/pelican/Main.java
@@ -1,6 +1,55 @@
 package winterskill.discord.pelican;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
 public class Main {
 	public static void main(String[] args) {
+		String token = "";
+		
+		// lecture du fichier .token.txt qui contient le token
+		File tokenFile = new File(".token.txt");
+		FileInputStream fis = null;
+		
+		try {
+			fis = new FileInputStream(tokenFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		byte[] buf = new byte[8];
+		int    n   = 0;
+		
+		try {
+			while ((n = fis.read(buf)) >= 0) {
+				for (byte bit : buf) {
+					token += new StringBuilder().append("").append((char) bit).toString();
+				}
+				
+				buf = new byte[8];
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+		token = token.trim();
+		System.out.print(token);
+		
+		// fin de la lecture de .token.txt
+		
+		try {
+			JDA jda = new JDABuilder(AccountType.BOT).setToken(token).buildAsync();
+		} catch (LoginException e) {
+			e.printStackTrace();
+			return;
+		}
 	}
 }
